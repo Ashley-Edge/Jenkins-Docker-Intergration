c84edf5e63bcaeb80b9ed94a7ddda3844a0de0bb
Ashley Edge <66746665+Ashley-Edge@users.noreply.github.com>
2021-02-17 11:47:20 +0000

    Update Dashboard.vue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -29,8 +29,10 @@ export default {
 <style scoped>
 
 .headerbox{
-  background: green;
+  background: lightblue;
   margin: 0 13em;
-  border-top-left-radius: 1em;
+  border-top-left-radius: 1em;  
   border-top-right-radius: 1em;
 }
+
+</style>
